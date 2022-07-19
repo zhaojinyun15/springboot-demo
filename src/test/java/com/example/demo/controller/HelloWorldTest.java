7019c8a643e9302c9e7c9f0b8b105fb182d1a1a0
zhaojinyun15 <zhaojinyun15@outlook.com>
2022-07-19 20:02:54 +0800

    feat: test ParameterizedTest and ValueSource

--- src/test/java/com/example/demo/controller/HelloWorldTest.java
+++ src/test/java/com/example/demo/controller/HelloWorldTest.java
@@ -3,7 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.DemoApplication;
 import com.example.demo.util.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,9 +15,10 @@ public class HelloWorldTest {
     @Autowired
     private HelloWorld helloWorld;
 
-    @Test
-    public void selectTest1Test() {
-        Result result = helloWorld.selectTest1(5);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    public void selectTest1Test(Integer id) {
+        Result result = helloWorld.selectTest1(id);
         log.debug("result is: {}", result);
     }
 }
